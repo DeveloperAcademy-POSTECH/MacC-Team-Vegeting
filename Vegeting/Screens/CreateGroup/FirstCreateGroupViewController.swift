b299d77c4d6dad83ca163f57dbaa01316de19a6b
ccdkk <cdk243@gmail.com>
2022-10-22 16:47:15 +0900

    [chore] CreateGroup 폴더 추가 (#9)

---
+++ Vegeting/Screens/CreateGroup/FirstCreateGroupViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FirstCreateGroupViewController.swift
+//  Vegeting
+//
+//  Created by 최동권 on 2022/10/22.
+//
+
+import UIKit
+
+class FirstCreateGroupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
