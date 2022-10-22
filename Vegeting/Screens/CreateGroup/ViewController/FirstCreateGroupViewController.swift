592ee633b2249c7d00f8493be8267ebfe2edfdb8
ccdkk <cdk243@gmail.com>
2022-10-22 22:15:30 +0900

    [chore] 폴더 네이밍 변경 & 주석 삭제 (#12)

--- Vegeting/Screens/CreateGroup/ViewController/FirstCreateGroupViewController.swift
+++ Vegeting/Screens/CreateGroup/ViewController/FirstCreateGroupViewController.swift
@@ -11,8 +11,6 @@ class FirstCreateGroupViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
 
