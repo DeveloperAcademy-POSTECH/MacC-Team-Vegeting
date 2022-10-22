188e374b934725f7725328a14b083b8025d13325
ccdkk <cdk243@gmail.com>
2022-10-22 16:52:42 +0900

    [chore] Global 폴더에 UIComponent 폴더 추가, GroupCategory 관련 swift 파일 추가 (#9)

---
+++ Vegeting/Global/UIComponent/Cell/GroupCategoryCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  GroupCategoryCollectionViewCell.swift
+//  Vegeting
+//
+//  Created by 최동권 on 2022/10/22.
+//
+
+import UIKit
+
+class GroupCategoryCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ Vegeting/Global/UIComponent/GroupCategoryView.swift
@@ -0,0 +1,20 @@
+//
+//  GroupCategoryView.swift
+//  Vegeting
+//
+//  Created by 최동권 on 2022/10/22.
+//
+
+import UIKit
+
+class GroupCategoryView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
