09ac61786798de17b5c411075f49dd479959b463
ccdkk <cdk243@gmail.com>
2022-10-22 18:11:06 +0900

    [add] GroupCategoryCell UI 구현 (#9) - UIView+Extension에 shadow를 넣어주는 함수를 추가하였습니다.

---
+++ Vegeting/Global/Extensions/UIView+Extension.swift
@@ -0,0 +1,22 @@
+//
+//  UIView+Extension.swift
+//  Vegeting
+//
+//  Created by 최동권 on 2022/10/22.
+//
+
+import UIKit
+
+extension UIView {
+    @discardableResult
+    func makeShadow(color: UIColor,
+                    opacity: Float,
+                    offset: CGSize,
+                    radius: CGFloat) -> Self {
+        layer.shadowColor = color.cgColor
+        layer.shadowOpacity = opacity
+        layer.shadowOffset = offset
+        layer.shadowRadius = radius
+        return self
+    }
+}
--- Vegeting/Global/UIComponent/Cell/GroupCategoryCollectionViewCell.swift
+++ Vegeting/Global/UIComponent/Cell/GroupCategoryCollectionViewCell.swift
@@ -9,4 +9,68 @@ import UIKit
 
 class GroupCategoryCollectionViewCell: UICollectionViewCell {
     
+    // MARK: - properties
+    
+    private let backgroundContentView: UIView = {
+        let view = UIView()
+        view.layer.masksToBounds = true
+        view.backgroundColor = .white
+        return view
+    }()
+    
+    private let itemLabel: UILabel = {
+        let label = UILabel()
+        label.font = .preferredFont(forTextStyle: .subheadline)
+        label.textAlignment = .center
+        return label
+    }()
+    
+    // MARK: - init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupLayout()
+        configureUI()
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - func
+    
+    private func setupLayout() {
+        addSubview(backgroundContentView)
+        backgroundContentView.constraint(to: self)
+        
+        backgroundContentView.addSubview(itemLabel)
+        let itemConstraints = itemLabel.constraint(leading: self.leadingAnchor,
+                                                   trailing: self.trailingAnchor,
+                                                   centerY: self.centerYAnchor,
+                                                   padding: UIEdgeInsets(top: 0, left: 14, bottom: 0, right: 14))
+        itemConstraints[.leading]?.priority = UILayoutPriority(999)
+        itemConstraints[.trailing]?.priority = UILayoutPriority(999)
+    }
+    
+    private func configureUI() {
+        layer.masksToBounds = false
+        let cellCornerRadius = (self.bounds.size.width * (self.bounds.size.height / self.bounds.size.width)) / 2
+        backgroundContentView.layer.cornerRadius = cellCornerRadius
+        
+        makeShadow(color: .black,
+                   opacity: 0.3,
+                   offset: CGSize(width: 0, height: 2),
+                   radius: 2)
+    }
+    
+    func applySelectedState(_ isSelected: Bool) {
+        backgroundContentView.backgroundColor = isSelected ? .black : .white
+        itemLabel.textColor = isSelected ? .white : .black
+        itemLabel.font = isSelected ? .preferredFont(forTextStyle: .subheadline, compatibleWith: .init(legibilityWeight: .bold)) : .preferredFont(forTextStyle:.subheadline)
+    }
+    
+    func setItemLabel(with itemText: String) {
+        itemLabel.text = itemText
+    }
 }
