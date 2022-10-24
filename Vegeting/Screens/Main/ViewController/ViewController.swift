e2eac8af9552bcfae0d3c29a869f4517f7792298
yudonlee <39371835+yudonlee@users.noreply.github.com>
2022-10-25 08:43:43 +0900

    [add]: mainVC 로그인 시 fullScreen Modal

--- Vegeting/Screens/Main/ViewController/ViewController.swift
+++ Vegeting/Screens/Main/ViewController/ViewController.swift
@@ -6,13 +6,25 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        
+        if Auth.auth().currentUser == nil {
+            let SignInViewController = UINavigationController(rootViewController: SignInViewController())
+            SignInViewController.modalPresentationStyle = .fullScreen
+            present(SignInViewController, animated: true)
+        } else {
+            print(Auth.auth().currentUser?.uid)
+        }
+    }
 
 }
 
