ad63a3bdcbd223ee3466ccee5336061559ccc032
kelly1422 <kelly1422@naver.com>
2022-10-25 00:52:07 +0900

    [feat] create photoPicker custom view (#29)

---
+++ Vegeting/Screens/CreateGroup/ViewController/SecondCreateGroupViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SecondCreateGroupViewController.swift
+//  Vegeting
+//
+//  Created by kelly on 2022/10/24.
+//
+
+import UIKit
+
+class SecondCreateGroupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
