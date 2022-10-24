a5d3950b122d1ab71e8761056956085b74958aa0
yudonlee <39371835+yudonlee@users.noreply.github.com>
2022-10-25 02:10:54 +0900

    [chore] import indent 수정 #23

--- Vegeting/Global/Models/Chat.swift
+++ Vegeting/Global/Models/Chat.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+
 import FirebaseFirestoreSwift
 
 struct Chat: Identifiable, Codable {
--- Vegeting/Global/Models/Club.swift
+++ Vegeting/Global/Models/Club.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+
 import FirebaseFirestoreSwift
 
 struct Club: Identifiable, Codable {
