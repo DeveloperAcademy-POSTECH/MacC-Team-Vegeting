f65dce6c936bf39b7b62b6797feed1ef0dd97f4d
yudonlee <39371835+yudonlee@users.noreply.github.com>
2022-10-26 08:26:02 +0900

    [chore] User변수명 변경

--- Vegeting/Global/Models/VFUser.swift
+++ Vegeting/Global/Models/VFUser.swift
@@ -9,7 +9,7 @@ import Foundation
 
 import FirebaseFirestoreSwift
 
-struct VUser: Identifiable, Codable {
+struct VFUser: Identifiable, Codable {
     @DocumentID var id: String?
     let userID: String
     let imageURL: String?
