d58172ce084c5ee32649825e5246e24588d6e63d
kelly1422 <kelly1422@naver.com>
2022-10-26 00:00:39 +0900

    [add] 모임 정보를 보여주는 UIStackView 커스텀뷰 생성 (#29)

--- Vegeting/Global/Component/GroupInfoView.swift
+++ Vegeting/Global/Component/GroupInfoView.swift
@@ -8,6 +8,30 @@
 import UIKit
 
 class GroupInfoView: UIStackView {
+    var categoryLabel: UILabel = {
+        let label = UILabel()
+        label.text = "맛집"
+        return label
+    }()
+    
+    var locationLabel: UILabel = {
+        let label = UILabel()
+        label.text = "서울시 동대문구"
+        return label
+    }()
+    
+    var dateLabel: UILabel = {
+        let label = UILabel()
+        label.text = "12월 19일"
+        return label
+    }()
+    
+    var capacityLabel: UILabel = {
+        let label = UILabel()
+        label.text = "2/4"
+        return label
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         configureUI()
@@ -19,8 +43,12 @@ class GroupInfoView: UIStackView {
     }
     
     private func configureUI() {
+        self.setHorizontalStack()
     }
     
     private func setupLayout() {
+        [categoryLabel, locationLabel, dateLabel, capacityLabel].forEach {
+           addArrangedSubviews($0)
+        }
     }
 }
