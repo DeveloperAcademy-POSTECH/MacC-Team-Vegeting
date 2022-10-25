cce08b2645c50a37368bed7d6ec30c1ad6d13c35
ccdkk <cdk243@gmail.com>
2022-10-25 12:51:55 +0900

    [set & feat] 지역 주소 검색 api 추가 & swfityJson spm 추가 (#28) - swiftyJson은 추후에 삭제할 수도 있음

---
+++ Vegeting/Global/UIComponent/Models/Place.swift
@@ -0,0 +1,15 @@
+//
+//  Place.swift
+//  Vegeting
+//
+//  Created by 최동권 on 2022/10/25.
+//
+
+import Foundation
+
+struct Place{
+    let placeName :String
+    let region: String
+    let longitudeX:String
+    let latitudeY:String
+}
--- Vegeting/Screens/CreateGroup/ViewController/LocationSearchingViewController.swift
+++ Vegeting/Screens/CreateGroup/ViewController/LocationSearchingViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 final class LocationSearchingViewController: UIViewController {
     
@@ -17,21 +19,24 @@ final class LocationSearchingViewController: UIViewController {
         return tableView
     }()
     
+    var resultList: [Place] = []
+    
     // MARK: - lifeCycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavigationBar()
         configureUI()
+        requestLocation()
     }
     
     // MARK: - func
     
     private func setupNavigationBar() {
         let backButton = UIBarButtonItem(image: ImageLiteral.backwardChevronSymbol,
-                                        style: .plain,
-                                        target: self,
-                                        action: #selector(touchUpAddButton))
+                                         style: .plain,
+                                         target: self,
+                                         action: #selector(touchUpAddButton))
         navigationItem.leftBarButtonItem = backButton
         
         let searchController = UISearchController(searchResultsController: nil)
@@ -39,10 +44,10 @@ final class LocationSearchingViewController: UIViewController {
         self.navigationItem.searchController = searchController
     }
     
-//    private func configureTableView() {
-//        resultTableView.delegate = self
-//        resultTableView.dataSource = self
-//    }
+    //    private func configureTableView() {
+    //        resultTableView.delegate = self
+    //        resultTableView.dataSource = self
+    //    }
     
     private func configureUI() {
         view.backgroundColor = .white
@@ -51,6 +56,47 @@ final class LocationSearchingViewController: UIViewController {
     @objc
     private func touchUpAddButton() {
         self.navigationController?.popViewController(animated: true)
+    }
+    
+    func requestLocation() {
+        let headers: HTTPHeaders = [
+            "Authorization": "KakaoAK 0af518ebd6f6d9b7b526a91fbabeadc1"
+        ]
+        
+        let parameters: [String: Any] = [
+            "query": "포항 효자동",
+            "page": 1,
+            "size": 10
+        ]
+        
+        Session.default.request("https://dapi.kakao.com/v2/local/search/address.json", method: .get,
+                                parameters: parameters, headers: headers)
+        .responseJSON(completionHandler: { response in
+            switch response.result {
+            case .success(let value):
+                if let detailsPlace = JSON(value)["documents"].array{
+                    for item in detailsPlace{
+                        let placeName = item["place_name"].string ?? ""
+//                        let firstRegion = item["address"]["region_1depth_name"].string ?? ""
+//                        let secondRegion = item["address"]["region_2depth_name"].string ?? ""
+//                        let thirdRegion = item["address"]["region_3depth_name"].string ?? ""
+                        let addressName = item["address"]["address_name"].string ?? ""
+//                        let roadAdressName = item["road_address_name"].string ?? ""
+                        let longitudeX = item["x"].string ?? ""
+                        let latitudeY = item["y"].string ?? ""
+                        self.resultList.append(Place(placeName: placeName,
+                                                     region: addressName,
+                                                     longitudeX: longitudeX,
+                                                     latitudeY: latitudeY))
+                    }
+                }
+                print(self.resultList)
+                
+            case .failure(let error):
+                print("에러")
+                print(error)
+            }
+        })
     }
 }
 
