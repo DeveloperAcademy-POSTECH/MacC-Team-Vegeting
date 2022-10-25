08cf2e55cbf9d8a33102b860a736aeb04cb3717a
yudonlee <39371835+yudonlee@users.noreply.github.com>
2022-10-25 16:16:30 +0900

    [chore] 파베 user와 구분위해 VUser로 변경

--- Vegeting/Global/Models/VUser.swift
+++ Vegeting/Global/Models/VUser.swift
@@ -9,7 +9,7 @@ import Foundation
 
 import FirebaseFirestoreSwift
 
-struct User: Identifiable, Codable {
+struct VUser: Identifiable, Codable {
     @DocumentID var id: String?
     let userID: String
     let imageURL: String?
