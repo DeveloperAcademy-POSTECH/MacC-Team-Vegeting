0afd21e6735cee541c5039e5cf559eead0ca505a
kelly1422 <kelly1422@naver.com>
2022-10-25 23:12:49 +0900

    [add] 모임 정보를 보여주는 UIStackView 커스텀뷰 생성 (#29)

---
+++ Vegeting/Global/Component/GroupInfoView.swift
@@ -0,0 +1,26 @@
+//
+//  GroupInfo.swift
+//  Vegeting
+//
+//  Created by kelly on 2022/10/25.
+//
+
+import UIKit
+
+class GroupInfoView: UIStackView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureUI()
+        setupLayout()
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureUI() {
+    }
+    
+    private func setupLayout() {
+    }
+}
