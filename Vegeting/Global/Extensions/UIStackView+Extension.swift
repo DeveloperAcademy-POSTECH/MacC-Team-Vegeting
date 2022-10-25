0c189c066d52cd2461385211b17a07512ca5c0f0
kelly1422 <kelly1422@naver.com>
2022-10-25 23:22:17 +0900

    [add] horizontal 스택뷰 설정 함수 extension (#29)

---
+++ Vegeting/Global/Extensions/UIStackView+Extension.swift
@@ -0,0 +1,16 @@
+//
+//  UIStackView+Extension.swift
+//  Vegeting
+//
+//  Created by kelly on 2022/10/25.
+//
+
+import UIKit
+
+extension UIStackView {
+    func setHorizontalStack() {
+        axis = .horizontal
+        alignment = .center
+        distribution = .fillProportionally
+    }
+}
