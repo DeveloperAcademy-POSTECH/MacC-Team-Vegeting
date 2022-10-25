211c4ea4bb576b196aa464e784391e9759df0d7f
kelly1422 <kelly1422@naver.com>
2022-10-25 18:33:50 +0900

    [add] SecondCreateGroupViewController에서 사용할 string (#29)

--- Vegeting/Global/Literals/StringLiteral.swift
+++ Vegeting/Global/Literals/StringLiteral.swift
@@ -11,4 +11,8 @@ enum StringLiteral {
     
     static let exampleString = "안녕하세요"
     
+    // MARK: - SecondCreateGroupViewController
+    static let secondCreateGroupViewControllerPhoto = "사진을 선택해주세요"
+    static let secondCreateGroupViewControllerTitle = "모임의 제목을 입력해주세요"
+    static let secondCreateGroupViewControllerContent = "모임의 상세설명을 입력해주세요"
 }
