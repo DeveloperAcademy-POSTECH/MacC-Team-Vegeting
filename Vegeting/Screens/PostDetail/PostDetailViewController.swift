bcfca7f8377cc5a36136746a06d6c33de047a259
wonhui-kim <0512tini@gmail.com>
2022-10-26 20:09:25 +0900

    temporary commit

--- Vegeting/Screens/PostDetail/PostDetailViewController.swift
+++ Vegeting/Screens/PostDetail/PostDetailViewController.swift
@@ -7,10 +7,50 @@
 
 import UIKit
 
+public class VerticalAlignLabel: UILabel {
+    enum VerticalAlignment {
+        case top
+        case middle
+        case bottom
+    }
+
+    var verticalAlignment : VerticalAlignment = .top {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+
+    override public func textRect(forBounds bounds: CGRect, limitedToNumberOfLines: Int) -> CGRect {
+        let rect = super.textRect(forBounds: bounds, limitedToNumberOfLines: limitedToNumberOfLines)
+
+        if UIView.userInterfaceLayoutDirection(for: .unspecified) == .rightToLeft {
+            switch verticalAlignment {
+            case .top:
+                return CGRect(x: self.bounds.size.width - rect.size.width, y: bounds.origin.y, width: rect.size.width, height: rect.size.height)
+            case .middle:
+                return CGRect(x: self.bounds.size.width - rect.size.width, y: bounds.origin.y + (bounds.size.height - rect.size.height) / 2, width: rect.size.width, height: rect.size.height)
+            case .bottom:
+                return CGRect(x: self.bounds.size.width - rect.size.width, y: bounds.origin.y + (bounds.size.height - rect.size.height), width: rect.size.width, height: rect.size.height)
+            }
+        } else {
+            switch verticalAlignment {
+            case .top:
+                return CGRect(x: bounds.origin.x, y: bounds.origin.y, width: rect.size.width, height: rect.size.height)
+            case .middle:
+                return CGRect(x: bounds.origin.x, y: bounds.origin.y + (bounds.size.height - rect.size.height) / 2, width: rect.size.width, height: rect.size.height)
+            case .bottom:
+                return CGRect(x: bounds.origin.x, y: bounds.origin.y + (bounds.size.height - rect.size.height), width: rect.size.width, height: rect.size.height)
+            }
+        }
+    }
+
+    override public func drawText(in rect: CGRect) {
+        let r = self.textRect(forBounds: rect, limitedToNumberOfLines: self.numberOfLines)
+        super.drawText(in: r)
+    }
+}
+
 final class PostDetailViewController: UIViewController {
-    
-    let profileImages = ["profile1.jpg", "profile2.jpg", "profile3.jpg", "profile4.jpg", "profile5.jpg"]
-    let participantsNames = ["거북짱", "내가 올해 수영왕", "양송이좋아", "베프", "고수러버"]
     
     private let coverImageView: UIImageView = {
         let imageView = UIImageView()
@@ -21,76 +61,72 @@ final class PostDetailViewController: UIViewController {
         return imageView
     }()
     
+    private let categoryLabel: UIButton = {
+        let button = UIButton()
+        button.setTitle("맛집", for: .normal)
+        button.setTitleColor(.label, for: .normal)
+        button.backgroundColor = .lightGray
+        button.layer.cornerRadius = 15
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     private let titleLabel: UILabel = {
         let label = UILabel()
         label.text = "남미플랜트랩하고 거북이 먹으러 가요"
-        label.font = .systemFont(ofSize: 20, weight: .bold)
-        label.backgroundColor = .orange
+        label.font = .systemFont(ofSize: 20, weight: .semibold)
         label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-    
-    private let categoryLabel: UILabel = {
-        let label = UILabel()
-        label.text = "맛집"
-        label.backgroundColor = .gray
         return label
     }()
     
     private let locationLabel: UILabel = {
         let label = UILabel()
         label.text = "서울시 동대문구"
-        label.backgroundColor = .gray
+        label.font = .systemFont(ofSize: 15, weight: .thin)
+        label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
     private let dateLabel: UILabel = {
         let label = UILabel()
-        label.text = "12월 19일"
-        label.backgroundColor = .gray
+        label.text = "· 12월 19일"
+        label.font = .systemFont(ofSize: 15, weight: .thin)
+        label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
-    private let capacityLabel: UILabel = {
-        let label = UILabel()
-        label.text = "5/6"
-        label.backgroundColor = .gray
-        return label
-    }()
-    
-    private let stackView: UIStackView = {
-        let stackView = UIStackView()
-        stackView.isUserInteractionEnabled = false
-        stackView.axis = .horizontal
-        stackView.alignment = .center
-        stackView.distribution = .fillProportionally
-        stackView.spacing = 2
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        return stackView
-    }()
-    
-    private let contentTextLabel: UILabel = {
-        let label = UILabel()
+    private let contentTextLabel: VerticalAlignLabel = {
+        let label = VerticalAlignLabel()
         label.numberOfLines = 0
-        label.text = "저번에 너무 가고싶엇는데~~ 긴급휴무로 못 가서 너무 아쉬웠어여. 유명 맛집이라던데 갔다가 옆에 있는 거북이까지 갔다 오면 좋을 것 같아요!! 날짜는 10월 내에 토요일 중에 가능한 날짜 맞춰서 다녀오려고 해요~ㅎㅎ 같이 맛난 점심먹어용!"
-        label.backgroundColor = .purple
+        label.text = "저번에 너무 가고싶엇는데~~ 긴급휴무로 못 가서 너무 아쉬웠어여. 유명 맛집이라던데 갔다가 옆에 있는 거북이까지 갔다 오면 좋을 것 같아요!! 날짜는 10월 내에 토요일 중에 가능한 날짜 맞춰서 다녀오려고 해요~ㅎㅎ 같이 맛난 점심먹어용! 나머지는 긴내용을 채우귀 위함위하다다다다 로렌아킬리나 짱~!!!! 너무너무 좋아요. 한국에서도 콘서트 열어주세요,,,,"
+        label.font = .systemFont(ofSize: 15, weight: .regular)
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
     private let participantsLabel: UILabel = {
         let label = UILabel()
-        label.text = "참여하는 회원 5/6"
+        label.text = "참여하는 회원"
         label.font = .systemFont(ofSize: 20, weight: .semibold)
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
+    private let capacityLabel: UILabel = {
+        let label = UILabel()
+        label.text = "5/6"
+        label.font = .systemFont(ofSize: 20, weight: .semibold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let profileImages = ["profile1.jpg", "profile2.jpg", "profile3.jpg", "profile4.jpg", "profile5.jpg"]
+    let participantsNames = ["거북짱", "내가 올해 수영왕", "양송이좋아", "베프", "고수러버"]
+    
     private let profileCollectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.itemSize = CGSize(width: 80, height: 80)
         layout.scrollDirection = .horizontal
-        
     
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collectionView.register(ProfileCollectionViewCell.self, forCellWithReuseIdentifier: ProfileCollectionViewCell.identifier)
@@ -100,9 +136,10 @@ final class PostDetailViewController: UIViewController {
     
     private let enterButton: UIButton = {
         let button = UIButton()
-        button.setTitle("참여하기", for: .normal)
         button.backgroundColor = .black
+        button.setTitle("참여하기", for: .normal)
         button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 17)
         button.layer.cornerRadius = 8
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
@@ -132,8 +169,8 @@ final class PostDetailViewController: UIViewController {
     }
     
     private func configureAddSubviews() {
-        [coverImageView, titleLabel, stackView, contentTextLabel,
-         participantsLabel, profileCollectionView, enterButton].forEach { subView in
+        [coverImageView, categoryLabel, titleLabel, locationLabel, dateLabel, contentTextLabel,
+         participantsLabel, capacityLabel, profileCollectionView, enterButton].forEach { subView in
             view.addSubview(subView)
         }
     }
@@ -147,40 +184,55 @@ final class PostDetailViewController: UIViewController {
         ])
         
         NSLayoutConstraint.activate([
-            titleLabel.topAnchor.constraint(equalTo: coverImageView.bottomAnchor, constant: 20),
+            categoryLabel.topAnchor.constraint(equalTo: coverImageView.bottomAnchor, constant: 15),
+            categoryLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            categoryLabel.widthAnchor.constraint(equalToConstant: 66),
+            categoryLabel.heightAnchor.constraint(equalToConstant: 31)
+        ])
+        
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: categoryLabel.bottomAnchor, constant: 12),
             titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
         ])
         
-        stackView.addArrangedSubviews(categoryLabel, locationLabel, dateLabel, capacityLabel)
         NSLayoutConstraint.activate([
-            stackView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20),
-            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+            locationLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 12),
+            locationLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20)
         ])
         
         NSLayoutConstraint.activate([
-            contentTextLabel.topAnchor.constraint(equalTo: stackView.bottomAnchor, constant: 20),
+            dateLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 12),
+            dateLabel.leadingAnchor.constraint(equalTo: locationLabel.trailingAnchor, constant: 5)
+        ])
+        
+        NSLayoutConstraint.activate([
+            contentTextLabel.topAnchor.constraint(equalTo: locationLabel.bottomAnchor, constant: 12),
             contentTextLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
             contentTextLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
             contentTextLabel.heightAnchor.constraint(equalToConstant: 180)
         ])
         
         NSLayoutConstraint.activate([
-            participantsLabel.bottomAnchor.constraint(equalTo: profileCollectionView.topAnchor),
+            participantsLabel.topAnchor.constraint(equalTo: contentTextLabel.bottomAnchor, constant: 15),
             participantsLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20)
         ])
         
         NSLayoutConstraint.activate([
-            profileCollectionView.bottomAnchor.constraint(equalTo: enterButton.topAnchor, constant: -20),
-            profileCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            profileCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-            profileCollectionView.heightAnchor.constraint(equalToConstant: 120)
+            capacityLabel.topAnchor.constraint(equalTo: contentTextLabel.bottomAnchor, constant: 15),
+            capacityLabel.leadingAnchor.constraint(equalTo: participantsLabel.trailingAnchor, constant: 10)
         ])
         
         NSLayoutConstraint.activate([
-            enterButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
+            profileCollectionView.topAnchor.constraint(equalTo: participantsLabel.bottomAnchor),
+            profileCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            profileCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            profileCollectionView.heightAnchor.constraint(equalToConstant: 115)
+        ])
+        
+        NSLayoutConstraint.activate([
+            enterButton.topAnchor.constraint(equalTo: profileCollectionView.bottomAnchor, constant: 15),
             enterButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            enterButton.widthAnchor.constraint(equalToConstant: 350),
+            enterButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
             enterButton.heightAnchor.constraint(equalToConstant: 48)
         ])
     }
