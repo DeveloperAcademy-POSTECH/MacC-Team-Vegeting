b84e12feb095da692da67205321eb2431095e627
wonhui-kim <0512tini@gmail.com>
2022-10-26 14:57:31 +0900

    [feat]: profileCollectionView 구현 (#40)

--- Vegeting/Screens/PostDetail/PostDetailViewController.swift
+++ Vegeting/Screens/PostDetail/PostDetailViewController.swift
@@ -7,117 +7,181 @@
 
 import UIKit
 
-class PostDetailViewController: UIViewController {
+final class PostDetailViewController: UIViewController {
     
-    private lazy var coverImageView: UIImageView = {
+    let profileImages = ["profile1.jpg", "profile2.jpg", "profile3.jpg", "profile4.jpg", "profile5.jpg"]
+    let participantsNames = ["거북짱", "내가 올해 수영왕", "양송이좋아", "베프", "고수러버"]
+    
+    private let coverImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFill
         imageView.clipsToBounds = true
         imageView.image = UIImage(named: "coverImage")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
     
-    private lazy var titleLabel: UILabel = {
+    private let titleLabel: UILabel = {
         let label = UILabel()
-        label.font = .systemFont(ofSize: 18, weight: .bold)
         label.text = "남미플랜트랩하고 거북이 먹으러 가요"
-        
+        label.font = .systemFont(ofSize: 20, weight: .bold)
+        label.backgroundColor = .orange
+        label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
-    private lazy var categoryLabel: UILabel = {
+    private let categoryLabel: UILabel = {
         let label = UILabel()
         label.text = "맛집"
+        label.backgroundColor = .gray
         return label
     }()
     
-    private lazy var locationLabel: UILabel = {
+    private let locationLabel: UILabel = {
         let label = UILabel()
         label.text = "서울시 동대문구"
+        label.backgroundColor = .gray
         return label
     }()
     
-    private lazy var dateLabel: UILabel = {
+    private let dateLabel: UILabel = {
         let label = UILabel()
         label.text = "12월 19일"
+        label.backgroundColor = .gray
         return label
     }()
     
-    private lazy var capacityLabel: UILabel = {
+    private let capacityLabel: UILabel = {
         let label = UILabel()
         label.text = "2/4"
+        label.backgroundColor = .gray
         return label
     }()
     
-    private lazy var stackView: UIStackView = {
+    private let stackView: UIStackView = {
         let stackView = UIStackView()
+        stackView.isUserInteractionEnabled = false
         stackView.axis = .horizontal
         stackView.alignment = .center
         stackView.distribution = .fillProportionally
         stackView.spacing = 2
-//        stackView.backgroundColor = .blue
+        stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
     }()
     
-    private lazy var contentTextLabel: UILabel = {
+    private let contentTextLabel: UILabel = {
         let label = UILabel()
         label.numberOfLines = 0
         label.text = "저번에 너무 가고싶엇는데~~ 긴급휴무로 못 가서 너무 아쉬웠어여. 유명 맛집이라던데 갔다가 옆에 있는 거북이까지 갔다 오면 좋을 것 같아요!! 날짜는 10월 내에 토요일 중에 가능한 날짜 맞춰서 다녀오려고 해요~ㅎㅎ 같이 맛난 점심먹어용!"
-//        label.backgroundColor = .purple
+        label.backgroundColor = .purple
+        label.translatesAutoresizingMaskIntoConstraints = false
         return label
+    }()
+    
+    private let participantsLabel: UILabel = {
+        let label = UILabel()
+        label.text = "참여하는 회원"
+        label.font = .systemFont(ofSize: 20, weight: .semibold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let profileCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: 80, height: 80)
+        layout.scrollDirection = .horizontal
+        
+    
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(ProfileCollectionViewCell.self, forCellWithReuseIdentifier: ProfileCollectionViewCell.identifier)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
+    
+    private let enterButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("참여하기", for: .normal)
+        button.backgroundColor = .black
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 8
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .white
+        profileCollectionView.delegate = self
+        profileCollectionView.dataSource = self
         
+        configureAddSubviews()
+        configureUI()
+        configureNavBar()
         setupLayout()
-        setupNavBar()
     }
     
-    private func setupNavBar() {
-        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "ellipsis"), style: .done, target: self, action: #selector(showActionSheet))
+    private func configureUI() {
+        view.backgroundColor = .white
+    }
+    
+    private func configureNavBar() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "ellipsis"),
+                                                            style: .done, target: self,
+                                                            action: #selector(showActionSheet))
         navigationController?.navigationBar.tintColor = .label
     }
     
+    private func configureAddSubviews() {
+        [coverImageView, titleLabel, stackView, contentTextLabel,
+         participantsLabel, profileCollectionView, enterButton].forEach { subView in
+            view.addSubview(subView)
+        }
+    }
+    
     private func setupLayout() {
-        self.view.addSubview(coverImageView)
-        coverImageView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            coverImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+            coverImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
             coverImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             coverImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             coverImageView.heightAnchor.constraint(equalToConstant: 200)
         ])
         
-        self.view.addSubview(titleLabel)
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             titleLabel.topAnchor.constraint(equalTo: coverImageView.bottomAnchor, constant: 20),
-            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20)
+            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
         ])
         
-        self.view.addSubview(stackView)
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        [categoryLabel, locationLabel, dateLabel, capacityLabel].map {
-            self.stackView.addArrangedSubviews($0)
-//            $0.heightAnchor.constraint(equalTo: $0.widthAnchor, multiplier: 1.0).isActive = true
-        }
+        stackView.addArrangedSubviews(categoryLabel, locationLabel, dateLabel, capacityLabel)
         NSLayoutConstraint.activate([
-            stackView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor),
+            stackView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20),
             stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-            stackView.heightAnchor.constraint(equalToConstant: 100)
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
         ])
         
-        self.view.addSubview(contentTextLabel)
-        contentTextLabel.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            contentTextLabel.topAnchor.constraint(equalTo: stackView.bottomAnchor),
+            contentTextLabel.topAnchor.constraint(equalTo: stackView.bottomAnchor, constant: 20),
             contentTextLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
             contentTextLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-            contentTextLabel.heightAnchor.constraint(equalToConstant: 200)
+            contentTextLabel.heightAnchor.constraint(equalToConstant: 180)
+        ])
+        
+        NSLayoutConstraint.activate([
+            participantsLabel.bottomAnchor.constraint(equalTo: profileCollectionView.topAnchor),
+            participantsLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20)
+        ])
+        
+        NSLayoutConstraint.activate([
+            profileCollectionView.bottomAnchor.constraint(equalTo: enterButton.topAnchor, constant: -20),
+            profileCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            profileCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            profileCollectionView.heightAnchor.constraint(equalToConstant: 120)
+        ])
+        
+        NSLayoutConstraint.activate([
+            enterButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
+            enterButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            enterButton.widthAnchor.constraint(equalToConstant: 350),
+            enterButton.heightAnchor.constraint(equalToConstant: 48)
         ])
     }
     
@@ -125,22 +189,36 @@ class PostDetailViewController: UIViewController {
     private func showActionSheet() {
         let actionSheet = UIAlertController(title: .none, message: .none, preferredStyle: .actionSheet)
         
-        let deletePost = UIAlertAction(title: "게시글 삭제", style: .default) { action in
+        let deletePostAlert = UIAlertAction(title: "게시글 삭제", style: .default) { action in
             //TODO: 게시글 삭제 action 연결
         }
         
-        let modifyPost = UIAlertAction(title: "게시글 수정", style: .default) { action in
+        let modifyPostAlert = UIAlertAction(title: "게시글 수정", style: .default) { action in
             //TODO: 게시글 수정 action 연결
         }
         
-        let cancel = UIAlertAction(title: "취소", style: .cancel) { action in
+        let cancelAlert = UIAlertAction(title: "취소", style: .cancel) { action in
             //TODO: 액션시트 취소 action 연결
         }
         
-        [deletePost, modifyPost, cancel].forEach { action in
+        [deletePostAlert, modifyPostAlert, cancelAlert].forEach { action in
             actionSheet.addAction(action)
         }
         
         present(actionSheet, animated: true, completion: nil)
+    }
+}
+
+extension PostDetailViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return profileImages.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ProfileCollectionViewCell.identifier, for: indexPath)
+                as? ProfileCollectionViewCell else { return UICollectionViewCell() }
+        cell.profileImage.image = UIImage(named: profileImages[indexPath.row])
+        cell.participantsName.text = participantsNames[indexPath.row]
+        return cell
     }
 }
---
+++ Vegeting/Screens/PostDetail/ProfileCollectionViewCell.swift
@@ -0,0 +1,62 @@
+//
+//  ProfileCollectionViewCell.swift
+//  Vegeting
+//
+//  Created by 김원희 on 2022/10/26.
+//
+
+import UIKit
+
+struct Constants {
+    static let profileImageSize = 80.0
+    static let profileImageCGSize = CGSize(width: Constants.profileImageSize, height: Constants.profileImageSize)
+    static let borderWidth = 2.0
+    static let spacing = 4.0
+}
+
+class ProfileCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier = "ProfileCollectionViewCell"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    let profileImage: UIImageView = {
+        let image = UIImageView()
+        image.layer.cornerRadius = (Constants.profileImageSize - Constants.spacing * 2) / 2.0
+        image.clipsToBounds = true
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
+    
+    let participantsName: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 15)
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    func setupLayout() {
+        contentView.addSubview(profileImage)
+        NSLayoutConstraint.activate([
+            profileImage.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: Constants.spacing),
+            profileImage.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -Constants.spacing),
+            profileImage.topAnchor.constraint(equalTo: contentView.topAnchor, constant: Constants.spacing),
+            profileImage.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -Constants.spacing)
+        ])
+        
+        contentView.addSubview(participantsName)
+        NSLayoutConstraint.activate([
+            participantsName.topAnchor.constraint(equalTo: profileImage.bottomAnchor, constant: 5),
+            participantsName.widthAnchor.constraint(equalToConstant: Constants.profileImageSize)
+        ])
+    }
+}
