60d3cfd881d033b867123ff7f4be761651206dfe
kelly1422 <kelly1422@naver.com>
2022-10-26 20:14:28 +0900

    [add] collectionview

--- Vegeting/Screens/GroupList/ViewController/GroupListViewController.swift
+++ Vegeting/Screens/GroupList/ViewController/GroupListViewController.swift
@@ -7,9 +7,37 @@
 
 import UIKit
 
-class GroupListViewController: UIViewController {
+class GroupListViewController: BaseViewController {
+    private lazy var collectionView: UICollectionView {
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    private func configureCollectionView() {
+        
+    }
+    
+}
 
+extension GroupListViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print(memberList[indexPath.item])
+    }
+}
+
+extension GroupListViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return memberList.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MemberCollectionViewCell.className, for: indexPath) as? MemberCollectionViewCell else {
+            assert(false, "Wrong Cell")
+            return UICollectionViewCell()
+        }
+        cell.memberLabel.text = memberList[indexPath.item]
+        return cell
     }
 }
