bcb292ba4ae738e3c9fc021c3cf738de5c1cd9ba
ccdkk <cdk243@gmail.com>
2022-10-26 20:35:03 +0900

    [del] api key 삭제 (#28)

--- Vegeting/Global/Literals/StringLiteral.swift
+++ Vegeting/Global/Literals/StringLiteral.swift
@@ -10,7 +10,6 @@ import Foundation
 enum StringLiteral {
     
     static let exampleString = "안녕하세요"
-    static let kakaoRestAPIKey = "KakaoAK 0af518ebd6f6d9b7b526a91fbabeadc1"
     static let kakaoRestAPIAddress = "https://dapi.kakao.com/v2/local/search/address.json"
     static let kakaoRestAPIKeyword = "https://dapi.kakao.com/v2/local/search/keyword.json"
     
