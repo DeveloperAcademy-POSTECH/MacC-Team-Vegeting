cb3de2e4efabdc07d033c2ec85efd9821687ec01
kelly1422 <kelly1422@naver.com>
2022-10-26 19:52:49 +0900

    [add] 모임 리스트 view controller 파일 추가 (#49)

---
+++ Vegeting/Screens/GroupList/ViewController/GroupListViewController.swift
@@ -0,0 +1,15 @@
+//
+//  GroupListViewController.swift
+//  Vegeting
+//
+//  Created by kelly on 2022/10/26.
+//
+
+import UIKit
+
+class GroupListViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
