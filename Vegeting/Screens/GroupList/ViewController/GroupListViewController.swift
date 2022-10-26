fba7c142a28ce166c90df7885e6f32734d28e644
kelly1422 <kelly1422@naver.com>
2022-10-26 22:54:55 +0900

    [add] 모집중인 클럽 콜렉션 뷰 셀 적용, datasource, delegate, folwlayout 설정 (#49)

--- Vegeting/Screens/GroupList/ViewController/GroupListViewController.swift
+++ Vegeting/Screens/GroupList/ViewController/GroupListViewController.swift
@@ -7,37 +7,87 @@
 
 import UIKit
 
-class GroupListViewController: BaseViewController {
-    private lazy var collectionView: UICollectionView {
-        
+class GroupListViewController: ViewController {
+    var clubList = [Club]() {
+        didSet {
+            collectionView.reloadData()
+        }
     }
+    
+    private lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.minimumInteritemSpacing = 8
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .clear
+        collectionView.register(ClubListCell.self, forCellWithReuseIdentifier: ClubListCell.className)
+        return collectionView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureCollectionView()
+        configureUI()
+        setupLayout()
     }
     
     private func configureCollectionView() {
-        
+        collectionView.delegate = self
+        collectionView.dataSource = self
     }
     
+    private func configureUI() {
+        view.backgroundColor = .backgroundColor
+    }
+    
+    private func setupLayout() {
+        [collectionView].forEach {
+            view.addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+    }
 }
 
 extension GroupListViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        print(memberList[indexPath.item])
+            print(clubList[indexPath.item])
     }
 }
 
 extension GroupListViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return memberList.count
+        return clubList.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MemberCollectionViewCell.className, for: indexPath) as? MemberCollectionViewCell else {
-            assert(false, "Wrong Cell")
-            return UICollectionViewCell()
-        }
-        cell.memberLabel.text = memberList[indexPath.item]
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ClubListCell.className, for: indexPath) as? ClubListCell else { return UICollectionViewCell() }
+        cell.configure(with: clubList[indexPath.item])
+        cell.layer.backgroundColor = UIColor.init(hex: "#e3e3e3").cgColor
         return cell
+    }
+}
+
+extension GroupListViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let sectionInsets = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        let width = collectionView.frame.width
+        let height = collectionView.frame.height
+        let itemsPerRow: CGFloat = 2
+        let widthPadding = sectionInsets.left * (itemsPerRow + 1)
+        let itemsPerColumn: CGFloat = 3.5
+        let heightPadding = sectionInsets.top * (itemsPerColumn + 1)
+        let cellWidth = (width - widthPadding) / itemsPerRow
+        let cellHeight = (height - heightPadding) / itemsPerColumn
+        
+        return CGSize(width: cellWidth, height: cellHeight)
     }
 }
