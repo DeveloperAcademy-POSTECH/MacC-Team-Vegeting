9e08584733ef900c688e62ef18df988d6d6bf2b7
kelly1422 <kelly1422@naver.com>
2022-11-02 02:35:41 +0900

    [chore] UIViewController 상속 (#49)

--- Vegeting/Screens/GroupList/ViewController/GroupListViewController.swift
+++ Vegeting/Screens/GroupList/ViewController/GroupListViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class GroupListViewController: ViewController {
+class GroupListViewController: UIViewController {
     var clubList = [Club]() {
         didSet {
             collectionView.reloadData()
