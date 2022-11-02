88e83c4f3e921c42d979aef948fc69cf6e9fb612
kelly1422 <kelly1422@naver.com>
2022-11-02 20:23:23 +0900

    [feat] 네비게이션바 숨기기 (#62)

--- Vegeting/Global/Supports/SceneDelegate.swift
+++ Vegeting/Global/Supports/SceneDelegate.swift
@@ -16,6 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
         let navigationViewController = UINavigationController(rootViewController: GroupListViewController())
+        navigationViewController.setNavigationBarHidden(true, animated: false)
         window.rootViewController = navigationViewController
         window.makeKeyAndVisible()
         self.window = window
