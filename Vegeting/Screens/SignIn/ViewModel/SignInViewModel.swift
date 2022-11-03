af93f2b389823112596ec295e71fe84e74d5ca95
yudonlee <39371835+yudonlee@users.noreply.github.com>
2022-11-04 01:20:19 +0900

    [feat]: 로그인 뷰모델 생성

--- Vegeting/Screens/SignIn/ViewModel/SignInViewModel.swift
+++ Vegeting/Screens/SignIn/ViewModel/SignInViewModel.swift
@@ -6,38 +6,68 @@
 //
 
 import Combine
+import CryptoKit
 import Foundation
 
 import Firebase
 
-final class SignInViewModel: ObservableObject {
+final class SignInViewModel {
     
-    @Published var email: String?
-    @Published var password: String?
-    @Published var isRegistrationFormValid: Bool = false
-    @Published var user: User?
-    
-    private var cancelBag: Set<AnyCancellable> = []
-    
-    func validateRegistrationForm() {
-        guard let email = email, let password = password else {
-            isRegistrationFormValid = false
-            return
-        }
-        
-        isRegistrationFormValid = email.count >= 4 && password.count >= 8
+    enum Input {
+        case appleSignInButtonTapped(credential: OAuthCredential)
+        case kakaoSignInButtonTapped
     }
     
-    func createUser() {
-        guard let email = email, let password = password else {
-            return
-        }
-        
-        FirebaseManager.shared.registerUser(email: email + "@MacVegeting.com", password: password)
-            .sink { _ in
+    enum Output {
+        case isFirstSignIn
+        case isAlreadySignIn
+        case isSignInFailed(error: Error)
+    }
+    
+    private let output: PassthroughSubject<Output, Never> = .init()
+    private var cancellables =  Set<AnyCancellable>()
+    
+    private var credential: OAuthCredential?
+    private var user: User?
+    
+    func transform(input: AnyPublisher<Input, Never>) -> AnyPublisher<Output, Never> {
+        input.sink { [weak self] event in
+            switch event {
+            case .appleSignInButtonTapped(let credential):
+                self?.credential = credential
+                self?.appleSignIn()
+            case .kakaoSignInButtonTapped:
+                print("kakaoButton")
+            }
+        }.store(in: &cancellables)
+        return output.eraseToAnyPublisher()
+    }
+    
+    func appleSignIn() {
+        guard let credential = credential else { return }
+
+        AuthManager.shared.signInUser(with: credential)
+            .sink { [weak self] completion in
+                if case .failure(let error) = completion{
+                    self?.output.send(.isSignInFailed(error: error))
+                }
             } receiveValue: { [weak self] user in
                 self?.user = user
-            }
-            .store(in: &cancelBag)
+                self?.isAccountAlreadyRegisted()
+            }.store(in: &self.cancellables)
+        
     }
+    
+    func isAccountAlreadyRegistered() {
+        guard let user = user else { return }
+        FirebaseManager.shared.isUserAlreadyExisted(user: user)
+            .sink { [weak self] completion in
+                if case .failure(let error) = completion {
+                    self?.output.send(.isSignInFailed(error: error))
+                }
+            } receiveValue: { [weak self] status in
+                self?.output.send(status ? .isAlreadySignIn : .isFirstSignIn)
+            }.store(in: &cancellables)
+    }
+    
 }
