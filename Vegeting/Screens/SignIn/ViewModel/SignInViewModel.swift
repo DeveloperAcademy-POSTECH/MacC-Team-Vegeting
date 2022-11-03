86938f2df0e35fb2568f0abb7ae3c1d4f5a819e7
yudonlee <39371835+yudonlee@users.noreply.github.com>
2022-11-04 08:20:32 +0900

    [feat]: viewModel에서 appleID 받도록 설정

--- Vegeting/Screens/SignIn/ViewModel/SignInViewModel.swift
+++ Vegeting/Screens/SignIn/ViewModel/SignInViewModel.swift
@@ -14,7 +14,7 @@ import Firebase
 final class SignInViewModel {
     
     enum Input {
-        case appleSignInButtonTapped(credential: OAuthCredential)
+        case appleSignInButtonTapped(tokenID: String)
         case kakaoSignInButtonTapped
     }
     
@@ -27,14 +27,14 @@ final class SignInViewModel {
     private let output: PassthroughSubject<Output, Never> = .init()
     private var cancellables =  Set<AnyCancellable>()
     
-    private var credential: OAuthCredential?
+    private var tokenID: String?
     private var user: User?
     
     func transform(input: AnyPublisher<Input, Never>) -> AnyPublisher<Output, Never> {
         input.sink { [weak self] event in
             switch event {
-            case .appleSignInButtonTapped(let credential):
-                self?.credential = credential
+            case .appleSignInButtonTapped(let tokenID):
+                self?.tokenID = tokenID
                 self?.appleSignIn()
             case .kakaoSignInButtonTapped:
                 print("kakaoButton")
@@ -44,8 +44,11 @@ final class SignInViewModel {
     }
     
     func appleSignIn() {
-        guard let credential = credential else { return }
-
+        guard let tokenID = tokenID else { return }
+        
+        let nonce = sha256(randomNonceString())
+        let credential = OAuthProvider.credential(withProviderID: "apple.com", idToken: tokenID, rawNonce: nonce)
+        
         AuthManager.shared.signInUser(with: credential)
             .sink { [weak self] completion in
                 if case .failure(let error) = completion{
@@ -68,6 +71,52 @@ final class SignInViewModel {
             } receiveValue: { [weak self] status in
                 self?.output.send(status ? .isAlreadySignIn : .isFirstSignIn)
             }.store(in: &cancellables)
+    }
+    
+    // Firebase에 애플 로그인을 위한 함수들(Firebase에서 유래)
+    private func randomNonceString(length: Int = 32) -> String {
+      precondition(length > 0)
+      let charset: [Character] =
+        Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+      var result = ""
+      var remainingLength = length
+
+      while remainingLength > 0 {
+        let randoms: [UInt8] = (0 ..< 16).map { _ in
+          var random: UInt8 = 0
+          let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
+          if errorCode != errSecSuccess {
+            fatalError(
+              "Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)"
+            )
+          }
+          return random
+        }
+
+        randoms.forEach { random in
+          if remainingLength == 0 {
+            return
+          }
+
+          if random < charset.count {
+            result.append(charset[Int(random)])
+            remainingLength -= 1
+          }
+        }
+      }
+
+      return result
+    }
+
+//    Firebase에 애플 로그인을 위한 함수들(Firebase에서 유래)
+    private func sha256(_ input: String) -> String {
+      let inputData = Data(input.utf8)
+      let hashedData = SHA256.hash(data: inputData)
+      let hashString = hashedData.compactMap {
+        String(format: "%02x", $0)
+      }.joined()
+
+      return hashString
     }
     
 }
