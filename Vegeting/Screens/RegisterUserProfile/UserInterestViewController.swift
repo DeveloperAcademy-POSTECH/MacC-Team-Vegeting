3eb5537b8950adc25ef5a82b2765c0f5d49fd89d
ccdkk <cdk243@gmail.com>
2022-11-06 17:30:51 +0900

    [add]: UserInterestingViewController 추가 (#84)

---
+++ Vegeting/Screens/RegisterUserProfile/UserInterestViewController.swift
@@ -0,0 +1,24 @@
+//
+//  UserInterestViewController.swift
+//  Vegeting
+//
+//  Created by 최동권 on 2022/11/06.
+//
+
+import UIKit
+
+class UserInterestViewController: UIViewController {
+    
+    private let progressBarImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "progress5")
+        return imageView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
