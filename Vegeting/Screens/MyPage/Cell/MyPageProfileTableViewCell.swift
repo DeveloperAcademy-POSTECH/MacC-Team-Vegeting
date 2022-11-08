2c9980fc6264e7b5db1952c3783a52a95d400a24
ccdkk <cdk243@gmail.com>
2022-11-08 16:19:43 +0900

    [add]: my-age profile section 구현 (#96)

--- Vegeting/Screens/MyPage/Cell/MyPageProfileTableViewCell.swift
+++ Vegeting/Screens/MyPage/Cell/MyPageProfileTableViewCell.swift
@@ -8,9 +8,50 @@
 import UIKit
 
 class MyPageProfileTableViewCell: UITableViewCell {
-
+    
+    private let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.layer.masksToBounds = true
+        imageView.layer.bounds.size = .init(width: 60, height: 60)
+        imageView.layer.cornerRadius = imageView.bounds.height / 2
+        return imageView
+    }()
+    
+    private let labelStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 1
+        return stackView
+    }()
+    
+    private let nicknameLabel: UILabel = {
+        let label = UILabel()
+        label.font = .preferredFont(forTextStyle: .headline, compatibleWith: .init(legibilityWeight: .bold))
+        return label
+    }()
+    
+    private let vegetarianStepLabel: UILabel = {
+        let label = UILabel()
+        label.font = .preferredFont(forTextStyle: .subheadline, compatibleWith: .init(legibilityWeight: .regular))
+        label.textColor = .systemGray2
+        return label
+    }()
+    
+    private lazy var editButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("프로필 편집", for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 13, weight: .medium)
+        button.setTitleColor(.gray, for: .normal)
+        button.setBackgroundColor(.systemGray5, for: .normal)
+        button.layer.masksToBounds = true
+        editButton.layer.cornerRadius = 7
+        return button
+    }()
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupLayout()
+        configureUI()
     }
     
     required init?(coder: NSCoder) {
@@ -19,8 +60,40 @@ class MyPageProfileTableViewCell: UITableViewCell {
     
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
-
+    
+    private func setupLayout() {
+        contentView.addSubviews(profileImageView, labelStackView, editButton)
+        labelStackView.addArrangedSubviews(nicknameLabel, vegetarianStepLabel)
+        
+        profileImageView.constraint(top: contentView.topAnchor,
+                                    leading: contentView.leadingAnchor,
+                                    bottom: contentView.bottomAnchor,
+                                    padding: UIEdgeInsets(top: 22, left: 20, bottom: 22, right: 0))
+        profileImageView.constraint(.widthAnchor, constant: 60)
+        profileImageView.constraint(.heightAnchor, constant: 60)
+        
+        labelStackView.constraint(leading: profileImageView.trailingAnchor,
+                                  centerY: contentView.centerYAnchor,
+                                  padding: UIEdgeInsets(top: 0, left: 14, bottom: 0, right: 0))
+        
+        editButton.constraint(trailing: contentView.trailingAnchor,
+                              centerY: contentView.centerYAnchor,
+                              padding: UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 20))
+        editButton.constraint(.widthAnchor, constant: 79)
+        editButton.constraint(.heightAnchor, constant: 33)
+        
+    }
+    
+    private func configureUI() {
+        
+    }
+    
+   func configure(image: String, nickName: String, step: String) {
+        profileImageView.image = UIImage(named: image)
+        nicknameLabel.text = nickName
+        vegetarianStepLabel.text = step
+    }
 }
--- Vegeting/Screens/MyPage/ViewController/MyPageViewController.swift
+++ Vegeting/Screens/MyPage/ViewController/MyPageViewController.swift
@@ -9,21 +9,58 @@ import UIKit
 
 class MyPageViewController: UIViewController {
     
-    private let tableView: UITableView = {
+    private lazy var tableView: UITableView = {
         let tableView = UITableView()
         tableView.showsVerticalScrollIndicator = false
         tableView.register(MyPageTableViewCell.self, forCellReuseIdentifier: MyPageTableViewCell.className)
         tableView.register(MyPageProfileTableViewCell.self, forCellReuseIdentifier: MyPageProfileTableViewCell.className)
+        tableView.delegate = self
+        tableView.dataSource = self
         return tableView
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupLayout()
 
-        // Do any additional setup after loading the view.
     }
 
     private func setupLayout() {
-        
+        view.addSubview(tableView)
+        tableView.constraint(top: view.safeAreaLayoutGuide.topAnchor,
+                             leading: view.leadingAnchor,
+                             bottom: view.bottomAnchor,
+                             trailing: view.trailingAnchor)
     }
+}
+
+extension MyPageViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        switch section {
+//        case 0:
+            return 1
+//        case 1: return 12
+//        default: return 0
+//        }
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        switch indexPath.section {
+//        case 0:
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: MyPageProfileTableViewCell.className, for: indexPath) as? MyPageProfileTableViewCell else { return UITableViewCell() }
+            cell.configure(image: "coverImage", nickName: "내가제일잘나과", step: "플렉시테리언")
+//        case 1: return 12
+//        default: return 0
+//        }
+        return cell
+    }
+    
+    
+}
+
+extension MyPageViewController: UITableViewDelegate {
+    
 }
