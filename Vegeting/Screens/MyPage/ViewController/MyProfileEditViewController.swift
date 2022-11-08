37d6938a758ae270fa25ba486710ee51e9ec75c2
ccdkk <cdk243@gmail.com>
2022-11-09 00:58:41 +0900

    [add]: 하이파이 일부 작업 (#96) - mainYellow, mainDisabledYellow color 추가 - bottombutton에 적용

---
+++ Vegeting/Screens/MyPage/ViewController/MyProfileEditViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MyProfileEditViewController.swift
+//  Vegeting
+//
+//  Created by 최동권 on 2022/11/09.
+//
+
+import UIKit
+
+class MyProfileEditViewController: UIViewController {
+
+    private let profileView = profileView()
+    
+    private let bottomButton = BottomButton()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
