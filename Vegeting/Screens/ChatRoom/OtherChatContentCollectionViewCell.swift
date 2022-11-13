f68456e9ac36c92fe8493a223d4b8f88d31c3f92
yudonlee <39371835+yudonlee@users.noreply.github.com>
2022-11-13 15:09:54 +0900

    [feat]: 나, 남이 보낸 메세지 cell 생성 및 분리(기존 하나의 파일 분리) #111

--- Vegeting/Screens/ChatRoom/OtherChatContentCollectionViewCell.swift
+++ Vegeting/Screens/ChatRoom/OtherChatContentCollectionViewCell.swift
@@ -11,12 +11,15 @@ enum SenderType: CaseIterable {
     case mine
     case other
 }
-class ChatRoomContentCollectionViewCell: UICollectionViewCell {
+
+
+class OtherChatContentCollectionViewCell: UICollectionViewCell {
     
-    static let identifier = "ChatRoomContentCollectionViewCell"
-    
-    var contentLabelAnchors: [NSLayoutConstraint]?
-    var dateTimeLabelAnchors: [NSLayoutConstraint]?
+    static let identifier = "OtherChatContentCollectionViewCell"
+        
+    private enum SizeLiteral: CGFloat {
+        case profileImageSize = 37.0
+    }
     
     private let backgroundPaddingView: UIView = {
         let view = UIView()
@@ -29,7 +32,7 @@ class ChatRoomContentCollectionViewCell: UICollectionViewCell {
     
     private let contentLabel: UILabel = {
         let label = UILabel()
-        label.lineBreakMode = .byWordWrapping
+        label.lineBreakMode = .byCharWrapping
         label.numberOfLines = 0
         label.font = UIFont.systemFont(ofSize: 16, weight: .regular)
         return label
@@ -43,44 +46,42 @@ class ChatRoomContentCollectionViewCell: UICollectionViewCell {
         return label
     }()
     
+    private let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        //        임시 컬러
+        imageView.backgroundColor = .gray.withAlphaComponent(0.5)
+        imageView.layer.masksToBounds = true
+        imageView.layer.cornerRadius = SizeLiteral.profileImageSize.rawValue / 2
+        imageView.clipsToBounds = true
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    private let profileUserNameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "초보 채식인"
+        label.font = UIFont.systemFont(ofSize: 15, weight: .regular)
+        return label
+    }()
+    
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-    
+        configureUI()
+        setupLayout()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    override func prepareForReuse() {
-        super.prepareForReuse()
-        guard let contentLabelAnchors = contentLabelAnchors, let dateTimeLabelAnchors = dateTimeLabelAnchors else { return }
-
-        [contentLabelAnchors, dateTimeLabelAnchors].forEach { constraints in
-            NSLayoutConstraint.deactivate(constraints)
-        }
-
-    }
-    
     func configure() {
-        contentLabel.text = randomTestText()
-        let senderType = randomTestSenderType()
-        
-        switch senderType {
-        case .mine:
-            setupComponentLayoutMine()
-        case .other:
-            setupComponentLayoutOther()
-        }
-        
-        configureUI()
-        setupLayout()
+        contentLabel.text = randomTestText()        
     }
 }
 
 // MARK: 채팅방 데이터를 위한 임시 함수
-extension ChatRoomContentCollectionViewCell {
+extension OtherChatContentCollectionViewCell {
     private func randomTestSenderType() -> SenderType {
         let randomSenderType = SenderType.allCases.randomElement()!
         return randomSenderType
@@ -94,38 +95,21 @@ extension ChatRoomContentCollectionViewCell {
 }
 
 // MARK: Layout 관련 함수
-extension ChatRoomContentCollectionViewCell {
-    
-    private func setupComponentLayoutMine() {
-        contentLabelAnchors = [
-            contentLabel.leadingAnchor.constraint(greaterThanOrEqualTo: contentView.leadingAnchor, constant: 160),
-            contentLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -37),
-        ]
-        
-        dateTimeLabelAnchors = [
-            dateTimeLabel.trailingAnchor.constraint(equalTo: backgroundPaddingView.leadingAnchor, constant: -8),
-        ]
-    }
-    
-    private func setupComponentLayoutOther() {
-        contentLabelAnchors = [
-            contentLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 78),
-            contentLabel.trailingAnchor.constraint(lessThanOrEqualTo: contentView.trailingAnchor, constant: -66),
-        ]
-        
-        dateTimeLabelAnchors = [
-            dateTimeLabel.leadingAnchor.constraint(equalTo: backgroundPaddingView.trailingAnchor, constant: 8),
-        ]
-    }
-    
+extension OtherChatContentCollectionViewCell {
+
     private func configureUI() {
         contentView.addSubviews(backgroundPaddingView, contentLabel, dateTimeLabel)
+        contentView.addSubviews(profileImageView, profileUserNameLabel)
     }
     
     private func setupLayout() {
-        guard let contentLabelAnchors = contentLabelAnchors, let dateTimeLabelAnchors = dateTimeLabelAnchors else { return }
+        
+        setupProfileLayout()
+        
         let contentLabelConstraints = [
-            contentLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
+            contentLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 78),
+            contentLabel.trailingAnchor.constraint(lessThanOrEqualTo: contentView.trailingAnchor, constant: -66),
+            contentLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 36),
             contentLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8),
         ]
         
@@ -133,19 +117,40 @@ extension ChatRoomContentCollectionViewCell {
         let backgroundPaddingViewConstraints = [
             backgroundPaddingView.leadingAnchor.constraint(equalTo: contentLabel.leadingAnchor, constant: -16),
             backgroundPaddingView.trailingAnchor.constraint(equalTo: contentLabel.trailingAnchor, constant: 16),
-            backgroundPaddingView.topAnchor.constraint(equalTo: contentView.topAnchor),
-            backgroundPaddingView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+            backgroundPaddingView.topAnchor.constraint(equalTo: contentLabel.topAnchor, constant: -8),
+            backgroundPaddingView.bottomAnchor.constraint(equalTo: contentLabel.bottomAnchor, constant: 8)
             
         ]
         
         let dateTimeLabelConstraints = [
+            dateTimeLabel.leadingAnchor.constraint(equalTo: backgroundPaddingView.trailingAnchor, constant: 8),
             dateTimeLabel.bottomAnchor.constraint(equalTo: backgroundPaddingView.bottomAnchor)
         ]
-        
-        [contentLabelAnchors, contentLabelConstraints, dateTimeLabelAnchors, dateTimeLabelConstraints, backgroundPaddingViewConstraints].forEach { constraints in
+
+        [contentLabelConstraints, dateTimeLabelConstraints, backgroundPaddingViewConstraints].forEach { constraints in
             NSLayoutConstraint.activate(constraints)
         }
         
     }
     
+    private func setupProfileLayout() {
+        
+        let profileImageViewConstraints = [
+            profileImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+            profileImageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            profileImageView.widthAnchor.constraint(equalToConstant: SizeLiteral.profileImageSize.rawValue),
+            profileImageView.heightAnchor.constraint(equalToConstant: SizeLiteral.profileImageSize.rawValue)
+        ]
+        
+        let profileUserNameLabelConstraints = [
+            profileUserNameLabel.topAnchor.constraint(equalTo: profileImageView.topAnchor, constant: 4),
+            profileUserNameLabel.leadingAnchor.constraint(equalTo: profileImageView.trailingAnchor, constant: 8),
+        ]
+        
+        [profileImageViewConstraints, profileUserNameLabelConstraints].forEach { constraints in
+            NSLayoutConstraint.activate(constraints)
+        }
+        
+        
+    }
 }
