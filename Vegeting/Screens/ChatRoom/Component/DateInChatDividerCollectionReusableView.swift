b8e84c757015d38231cefe7f523969e064f8e3f7
yudonlee <39371835+yudonlee@users.noreply.github.com>
2022-11-13 17:48:47 +0900

    [feat] supplementary view 구현

---
+++ Vegeting/Screens/ChatRoom/Component/DateInChatDividerCollectionReusableView.swift
@@ -0,0 +1,44 @@
+//
+//  DateInChatDividerCollectionReusableView.swift
+//  Vegeting
+//
+//  Created by yudonlee on 2022/11/13.
+//
+
+import UIKit
+
+class DateInChatDividerCollectionReusableView: UICollectionReusableView {
+    
+    private let dateLabel: UILabel = {
+        let label = UILabel()
+        label.text = "2023년 9월 8일"
+        label.font = UIFont.systemFont(ofSize: 13, weight: .regular)
+        label.textColor = .gray.withAlphaComponent(0.1)
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureUI()
+        setupLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension DateInChatDividerCollectionReusableView {
+    
+    private func configureUI() {
+        addSubviews(dateLabel)
+    }
+    
+    private func setupLayout() {
+        let dateLabelConstraints = [
+            dateLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            dateLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor)
+        ]
+        NSLayoutConstraint.activate(dateLabelConstraints)
+    }
+}
