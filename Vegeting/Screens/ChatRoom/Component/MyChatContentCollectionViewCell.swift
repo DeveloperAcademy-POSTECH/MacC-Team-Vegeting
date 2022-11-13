b206168a373fd008bd8bf9b32bc8163001d96922
yudonlee <39371835+yudonlee@users.noreply.github.com>
2022-11-13 16:09:04 +0900

    [add]: 채팅방 랜덤 value 로직 vm으로 이동 #111

--- Vegeting/Screens/ChatRoom/Component/MyChatContentCollectionViewCell.swift
+++ Vegeting/Screens/ChatRoom/Component/MyChatContentCollectionViewCell.swift
@@ -47,23 +47,10 @@ class MyChatContentCollectionViewCell: UICollectionViewCell {
         fatalError("init(coder:) has not been implemented")
     }
     
-    func configure() {
-        contentLabel.text = randomTestText()
-    }
-}
-
-// MARK: 채팅방 데이터를 위한 임시 함수
-extension MyChatContentCollectionViewCell {
-    private func randomTestSenderType() -> SenderType {
-        let randomSenderType = SenderType.allCases.randomElement()!
-        return randomSenderType
+    func configure(with model: TemporaryMessage) {
+        contentLabel.text = model.messageContent
     }
     
-    private func randomTestText() -> String {
-        let labelCase = ["감자님이 저번에 말씀하셨던 곳이 거기인가요?", "와 저도 거기 진짜 가보고 싶었는데 ....... 리조또랑 퐁듀가 진짜 맛있고, 피클도 꼭 추가해야 한대요~!!!!", "고구마 함박집 창문동에 담달에 드디어 오픈 한대요!", "타임은 LG 클로이 서브봇을 로보틱스 분야 최고의 발명품으로 꼽았다. 라이다 센서와 3D 카메라를 사용해 혼잡한 공간에서도 안정적으로 주행하고, 66파운드(30kg) 물품을 연속 11시간 운반할 수 있다고 설명했다. 가정용 식물재배기인 LG 틔운은 식물을 잘 키우는 요령이 없는 사람도 집안에서 쉽게 재배할 수 있는 점을 강조했다."]
-        let idx = Int.random(in: 0...3)
-        return labelCase[idx]
-    }
 }
 
 // MARK: Layout 관련 함수
