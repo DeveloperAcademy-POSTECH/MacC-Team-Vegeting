2d9b0cacebd179490c2d6a59f35c70147cd4ed01
kelly1422 <kelly1422@naver.com>
2022-11-15 17:31:02 +0900

    [feat]: UIImage 익스텐션 - 이미지 사이즈 조절 (#122)

---
+++ Vegeting/Global/Extensions/UIImage+Extension.swift
@@ -0,0 +1,30 @@
+//
+//  UIImage+Extension.swift
+//  Vegeting
+//
+//  Created by kelly on 2022/11/15.
+//
+
+import UIKit
+
+extension UIImage {
+    var scaledToSafeUploadSize: UIImage? {
+        let maxImageSideLength: CGFloat = 480
+        let largerSide: CGFloat = max(size.width, size.height)
+        let ratioScale: CGFloat = largerSide > maxImageSideLength ? largerSide / maxImageSideLength : 1
+        let newImageSize = CGSize(width: size.width / ratioScale, height: size.height / ratioScale)
+        
+        return image(scaledTo: newImageSize)
+    }
+    
+    private func image(scaledTo size: CGSize) -> UIImage? {
+        defer {
+            UIGraphicsEndImageContext()
+        }
+        
+        UIGraphicsBeginImageContextWithOptions(size, true, 0)
+        draw(in: CGRect(origin: .zero, size: size))
+        
+        return UIGraphicsGetImageFromCurrentImageContext()
+    }
+}
