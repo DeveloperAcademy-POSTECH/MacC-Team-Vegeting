8af2e2a7d594c7c875e3c065b48c62856d0b93dd
yudonlee <39371835+yudonlee@users.noreply.github.com>
2022-11-20 13:02:59 +0900

    [feat]: TextView 줄수에 따라서 height 변경

--- Vegeting/Screens/ChatRoomDetail/ViewController/ChatRoomViewController.swift
+++ Vegeting/Screens/ChatRoomDetail/ViewController/ChatRoomViewController.swift
@@ -8,13 +8,30 @@
 import Combine
 import UIKit
 
+
+fileprivate enum keyboardHeightType: Int {
+    case zeroNewLine = 0
+    case oneNewLine = 1
+    case twoNewLine = 2
+    case other
+
+    var height: CGFloat {
+        switch self {
+        case .zeroNewLine, .oneNewLine, .twoNewLine:
+            return CGFloat(46 + self.rawValue * 22)
+        case .other:
+            return CGFloat(46 + 2 * 22)
+        }
+    }
+}
+
 final class ChatRoomViewController: UIViewController {
     
     private let viewModel = ChatRoomViewModel()
-    
     private var input: PassthroughSubject<ChatRoomViewModel.Input, Never> = .init()
     private var messageBubbles: [MessageBubble] = []
     private var cancellables =  Set<AnyCancellable>()
+    lazy private var messageTextViewHeightAnchor = messageTextView.heightAnchor.constraint(equalToConstant: 44)
     
     private let chatListCollectionView: UICollectionView = {
         
@@ -81,17 +98,24 @@ final class ChatRoomViewController: UIViewController {
     
     private func bind() {
         let output = viewModel.transform(input: input.eraseToAnyPublisher())
-        
+    
         output.sink { [weak self] event in
             switch event {
             case .localChatDataChanged(let messageBubbles), .serverChatDataChanged(let messageBubbles):
                 self?.messageBubbles = messageBubbles
                 DispatchQueue.main.async {
                     self?.chatListCollectionView.reloadData()
+                    let indexPath = IndexPath(item: messageBubbles.count - 1, section: 0)
+                    self?.chatListCollectionView.scrollToItem(at: indexPath, at: .bottom, animated: false)
                 }
             case .failToGetDataFromServer(let error):
                 print(error.localizedDescription)
             }
+        }.store(in: &cancellables)
+        
+        let textChangePublihser = NotificationCenter.default.publisher(for: UITextView.textDidChangeNotification)
+        textChangePublihser.sink { [weak self] _ in
+            self?.messageTextViewChanged()
         }.store(in: &cancellables)
     }
     
@@ -100,12 +124,19 @@ final class ChatRoomViewController: UIViewController {
     }
 }
 
-// MARK: target-action 함수
+// MARK: UI Input Event 담당 함수
 extension ChatRoomViewController {
     @objc
     private func sendButtonTapped(_ sender: Any) {
         input.send(.sendButtonTapped(text: messageTextView.text))
         messageTextView.text = ""
+    }
+    
+    private func messageTextViewChanged() {
+        let textViewNewLineCount = (messageTextView.text.filter { $0 == "\n" }.count)
+        if let lineCase = keyboardHeightType(rawValue: textViewNewLineCount) {
+            messageTextViewHeightAnchor.constant = lineCase.height
+        }
     }
 }
 
@@ -118,7 +149,7 @@ extension ChatRoomViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         let data = messageBubbles[indexPath.item]
-        switch data.senderType {
+        switch data.messageType {
         case .mine:
             guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MyChatContentCollectionViewCell.className, for: indexPath) as? MyChatContentCollectionViewCell else { return UICollectionViewCell() }
             cell.configure(with: data)
@@ -128,6 +159,7 @@ extension ChatRoomViewController: UICollectionViewDataSource {
             guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: OtherChatContentCollectionViewCell.className, for: indexPath) as? OtherChatContentCollectionViewCell else { return UICollectionViewCell() }
             cell.configure(with: data)
             return cell
+            
         case .date:
             guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MessageDateCollectionViewCell.className, for: indexPath) as? MessageDateCollectionViewCell else { return UICollectionViewCell() }
             cell.configure(message: data)
@@ -137,12 +169,14 @@ extension ChatRoomViewController: UICollectionViewDataSource {
 }
 
 extension ChatRoomViewController: UICollectionViewDelegate {
+    
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         view.endEditing(true)
     }
 }
 
 extension ChatRoomViewController {
+    
     private func configureUI() {
         view.addSubviews(chatListCollectionView,transferMessageStackView)
         view.backgroundColor = .systemBackground
@@ -154,19 +188,17 @@ extension ChatRoomViewController {
     private func setupLayout() {
         
         transferMessageStackView.addArrangedSubviews(plusButton, messageTextView, sendButton)
-        
-        messageTextView.heightAnchor.constraint(equalToConstant: 46).isActive = true
+        messageTextViewHeightAnchor.isActive = true
         let transferMessageStackViewConstraints = [
             transferMessageStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
             transferMessageStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-            transferMessageStackView.bottomAnchor.constraint(equalTo: view.keyboardLayoutGuide.topAnchor, constant: -12)
+            transferMessageStackView.bottomAnchor.constraint(equalTo: view.keyboardLayoutGuide.topAnchor, constant: -10),
         ]
-        
         
         let chatListCollectionViewConstraints = [
             chatListCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             chatListCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            chatListCollectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            chatListCollectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             chatListCollectionView.bottomAnchor.constraint(equalTo: transferMessageStackView.topAnchor, constant: -20)
         ]
         
