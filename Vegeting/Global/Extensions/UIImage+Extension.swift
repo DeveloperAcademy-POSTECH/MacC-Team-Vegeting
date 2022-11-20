eda1fd589ddd66a830443d478c02b811be3d72a5
kelly1422 <kelly1422@naver.com>
2022-11-20 22:45:49 +0900

    [fix]: UIImage 익스텐션 중복 파일 제거 (#122)

--- Vegeting/Global/Extensions/UIImage+Extension.swift
+++ Vegeting/Global/Extensions/UIImage+Extension.swift
@@ -1,5 +1,5 @@
 //
-//  UIImage+Extension.swift
+//  UIImage_Extension.swift
 //  Vegeting
 //
 //  Created by kelly on 2022/11/15.
@@ -8,6 +8,7 @@
 import UIKit
 
 extension UIImage {
+    // 해상도를 너무 높지 않게 조절, 파일 크기가 너무 높지 않도록
     var scaledToSafeUploadSize: UIImage? {
         let maxImageSideLength: CGFloat = 480
         let largerSide: CGFloat = max(size.width, size.height)
@@ -17,6 +18,7 @@ extension UIImage {
         return image(scaledTo: newImageSize)
     }
     
+    // 이미지를 입력받은 size에 맞춰 조절
     private func image(scaledTo size: CGSize) -> UIImage? {
         defer {
             UIGraphicsEndImageContext()
