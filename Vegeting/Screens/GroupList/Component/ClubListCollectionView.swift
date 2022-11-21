e5b6cd71715d75b547b321425d8cbac46cee413c
kelly1422 <kelly1422@naver.com>
2022-11-21 16:46:57 +0900

    [add]: 모임 컬렉션뷰 컴포넌트 작성 (#142)

--- Vegeting/Screens/GroupList/Component/ClubListCollectionView.swift
+++ Vegeting/Screens/GroupList/Component/ClubListCollectionView.swift
@@ -8,13 +8,65 @@
 import UIKit
 
 class ClubListCollectionView: UICollectionView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    private var clubList: [Club] = [] {
+        didSet {
+            DispatchQueue.main.async { [weak self] in
+                self?.reloadData()
+            }
+        }
     }
-    */
+    
+    // MARK: - init
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override init(frame: CGRect, collectionViewLayout layout: UICollectionViewLayout) {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.minimumInteritemSpacing = 20
+        layout.minimumLineSpacing = 30
+        layout.sectionInset = UIEdgeInsets(top: 20, left: 0, bottom: 20, right: 0)
+        super.init(frame: frame, collectionViewLayout: layout)
+        configureUI()
+        configureCollectionView()
+    }
+    
+    // MARK: - func
+    
+    private func configureUI() {
+        self.backgroundColor = .clear
+        self.register(ClubListCollectionViewCell.self, forCellWithReuseIdentifier: ClubListCollectionViewCell.className)
+        self.showsVerticalScrollIndicator = false
+    }
+    
+    private func configureCollectionView() {
+        self.dataSource = self
+    }
+    
+    func setClubList(clubList: [Club]) {
+        self.clubList = clubList
+    }
+}
 
+extension ClubListCollectionView: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return clubList.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ClubListCollectionViewCell.className, for: indexPath) as? ClubListCollectionViewCell else { return UICollectionViewCell() }
+        cell.configure(with: clubList[indexPath.item])
+        return cell
+    }
+}
+
+extension ClubListCollectionView: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: (collectionView.frame.width - 20) / 2, height: 165)
+    }
 }
