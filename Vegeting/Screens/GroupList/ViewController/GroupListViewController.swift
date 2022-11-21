9c34bd6d97733c55006316f3770b02bfa386908f
kelly1422 <kelly1422@naver.com>
2022-11-21 16:57:28 +0900

    [chore]: 메인 모임 컬레션뷰 컴포넌트화 적용 (#142)

--- Vegeting/Screens/GroupList/ViewController/GroupListViewController.swift
+++ Vegeting/Screens/GroupList/ViewController/GroupListViewController.swift
@@ -64,16 +64,8 @@ class GroupListViewController: UIViewController {
         return segmentedControl
     }()
     
-    private lazy var collectionView: UICollectionView = {
-        let layout = UICollectionViewFlowLayout()
-        layout.scrollDirection = .vertical
-        layout.minimumInteritemSpacing = 20
-        layout.minimumLineSpacing = 30
-        layout.sectionInset = UIEdgeInsets(top: 20, left: 0, bottom: 20, right: 0)
-        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        collectionView.backgroundColor = .clear
-        collectionView.register(ClubListCollectionViewCell.self, forCellWithReuseIdentifier: ClubListCollectionViewCell.className)
-        collectionView.showsVerticalScrollIndicator = false
+    private lazy var collectionView: ClubListCollectionView = {
+        let collectionView = ClubListCollectionView()
         return collectionView
     }()
     
@@ -82,7 +74,6 @@ class GroupListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setCustomNavigationBar()
-        configureCollectionView()
         configureUI()
         setupLayout()
     }
@@ -91,7 +82,8 @@ class GroupListViewController: UIViewController {
         super.viewWillAppear(animated)
         showTabBar()
         Task { [weak self] in
-            self?.clubList = await FirebaseManager.shared.requestClubInformation() ?? []
+            let clubList = await FirebaseManager.shared.requestClubInformation() ?? []
+            self?.collectionView.setClubList(clubList: clubList)
         }
     }
     
@@ -99,11 +91,6 @@ class GroupListViewController: UIViewController {
     
     private func showTabBar() {
         self.tabBarController?.tabBar.isHidden = false
-    }
-    
-    private func configureCollectionView() {
-        collectionView.delegate = self
-        collectionView.dataSource = self
     }
     
     private func configureUI() {
@@ -140,32 +127,5 @@ class GroupListViewController: UIViewController {
             collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
             collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
-    }
-}
-
-extension GroupListViewController: UICollectionViewDelegate {
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        print(clubList[indexPath.item])
-    }
-}
-
-extension GroupListViewController: UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return clubList.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView,
-                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ClubListCollectionViewCell.className, for: indexPath) as? ClubListCollectionViewCell else { return UICollectionViewCell() }
-        cell.configure(with: clubList[indexPath.item])
-        return cell
-    }
-}
-
-extension GroupListViewController: UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView,
-                        layout collectionViewLayout: UICollectionViewLayout,
-                        sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: (collectionView.frame.width - 20) / 2, height: 165)
     }
 }
