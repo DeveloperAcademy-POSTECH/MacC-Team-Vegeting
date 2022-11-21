44ea058bb99df551404f1de46852a4265a934f9a
kelly1422 <kelly1422@naver.com>
2022-11-21 17:11:16 +0900

    [feat]: 마감된 모임 표시, 장소 데이터 configure (#142)

--- Vegeting/Screens/GroupList/Component/ClubListCollectionViewCell.swift
+++ Vegeting/Screens/GroupList/Component/ClubListCollectionViewCell.swift
@@ -21,6 +21,16 @@ final class ClubListCollectionViewCell: UICollectionViewCell {
         return imageView
     }()
     
+    private lazy var invalidView: UILabel = {
+        let labelView = UILabel()
+        labelView.clipsToBounds = true
+        labelView.layer.cornerRadius = 8
+        labelView.textAlignment = .center
+        labelView.text = "마감"
+        labelView.font = .preferredFont(forTextStyle: .title3, compatibleWith: .init(legibilityWeight: .bold))
+        return labelView
+    }()
+    
     private lazy var titleLabel: UILabel = {
         let label = UILabel()
         label.numberOfLines = 1
@@ -45,13 +55,20 @@ final class ClubListCollectionViewCell: UICollectionViewCell {
     }
     
     private func setupLayout() {
-        addSubviews(coverImageView, categoryView, titleLabel, clubInfoLabel)
+        addSubviews(coverImageView, invalidView, categoryView, titleLabel, clubInfoLabel)
         
         NSLayoutConstraint.activate([
             coverImageView.topAnchor.constraint(equalTo: self.topAnchor),
             coverImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
             coverImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
             coverImageView.heightAnchor.constraint(equalToConstant: 87)
+        ])
+        
+        NSLayoutConstraint.activate([
+            invalidView.topAnchor.constraint(equalTo: self.topAnchor),
+            invalidView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            invalidView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            invalidView.heightAnchor.constraint(equalToConstant: 87)
         ])
         
         NSLayoutConstraint.activate([
@@ -81,7 +98,14 @@ final class ClubListCollectionViewCell: UICollectionViewCell {
         }
         categoryView.configure(text: item.clubCategory, backgroundColor: .vfGray4 )
         titleLabel.text = item.clubTitle
-        let participantsCount = item.participants?.count ?? 0
-        clubInfoLabel.text = "서울시 동작구ㆍ\(participantsCount)/\(item.maxNumberOfPeople) 모집"
+        let participantsCount = item.participants?.count ?? 1
+        clubInfoLabel.text = "\(item.placeToMeet)ㆍ\(participantsCount)/\(item.maxNumberOfPeople) 모집"
+        if Date() > item.dateToMeet || (participantsCount == item.maxNumberOfPeople) {
+            invalidView.textColor = UIColor(hex: "#FFD243", alpha: 1 )
+            invalidView.backgroundColor = UIColor(hex: "#000000", alpha: 0.5 )
+        } else {
+            invalidView.textColor = UIColor(hex: "#FFD243", alpha: 0 )
+            invalidView.backgroundColor = UIColor(hex: "#000000", alpha: 0 )
+        }
     }
 }
