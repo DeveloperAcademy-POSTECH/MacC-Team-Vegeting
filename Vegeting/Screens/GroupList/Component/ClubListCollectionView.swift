6c90038236d332ba1c63b7a21c529e019570149b
kelly1422 <kelly1422@naver.com>
2022-11-21 16:44:29 +0900

    [add]: 모임 컬렉션뷰 컴포넌트화할 파일 추가 (#142)

---
+++ Vegeting/Screens/GroupList/Component/ClubListCollectionView.swift
@@ -0,0 +1,20 @@
+//
+//  ClubListCollectionView.swift
+//  Vegeting
+//
+//  Created by kelly on 2022/11/21.
+//
+
+import UIKit
+
+class ClubListCollectionView: UICollectionView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
