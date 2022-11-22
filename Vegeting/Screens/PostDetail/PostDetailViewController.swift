e98f8000a6053bc24d6ac5d4dd3f513f6a58aee5
ccdkk <cdk243@gmail.com>
2022-11-22 14:56:13 +0900

    [del]: VerticalAlignLabel 삭제 (#153)

--- Vegeting/Screens/PostDetail/PostDetailViewController.swift
+++ Vegeting/Screens/PostDetail/PostDetailViewController.swift
@@ -6,49 +6,6 @@
 //
 import UIKit
 
-public class VerticalAlignLabel: UILabel {
-    enum VerticalAlignment {
-        case top
-        case middle
-        case bottom
-    }
-    
-    var verticalAlignment : VerticalAlignment = .top {
-        didSet {
-            setNeedsDisplay()
-        }
-    }
-    
-    override public func textRect(forBounds bounds: CGRect, limitedToNumberOfLines: Int) -> CGRect {
-        let rect = super.textRect(forBounds: bounds, limitedToNumberOfLines: limitedToNumberOfLines)
-        
-        if UIView.userInterfaceLayoutDirection(for: .unspecified) == .rightToLeft {
-            switch verticalAlignment {
-            case .top:
-                return CGRect(x: self.bounds.size.width - rect.size.width, y: bounds.origin.y, width: rect.size.width, height: rect.size.height)
-            case .middle:
-                return CGRect(x: self.bounds.size.width - rect.size.width, y: bounds.origin.y + (bounds.size.height - rect.size.height) / 2, width: rect.size.width, height: rect.size.height)
-            case .bottom:
-                return CGRect(x: self.bounds.size.width - rect.size.width, y: bounds.origin.y + (bounds.size.height - rect.size.height), width: rect.size.width, height: rect.size.height)
-            }
-        } else {
-            switch verticalAlignment {
-            case .top:
-                return CGRect(x: bounds.origin.x, y: bounds.origin.y, width: rect.size.width, height: rect.size.height)
-            case .middle:
-                return CGRect(x: bounds.origin.x, y: bounds.origin.y + (bounds.size.height - rect.size.height) / 2, width: rect.size.width, height: rect.size.height)
-            case .bottom:
-                return CGRect(x: bounds.origin.x, y: bounds.origin.y + (bounds.size.height - rect.size.height), width: rect.size.width, height: rect.size.height)
-            }
-        }
-    }
-    
-    override public func drawText(in rect: CGRect) {
-        let r = self.textRect(forBounds: rect, limitedToNumberOfLines: self.numberOfLines)
-        super.drawText(in: r)
-    }
-}
-
 final class PostDetailViewController: UIViewController {
     
     // MARK: - properties
@@ -109,8 +66,8 @@ final class PostDetailViewController: UIViewController {
         return label
     }()
     
-    private lazy var contentTextLabel: VerticalAlignLabel = {
-        let label = VerticalAlignLabel()
+    private lazy var contentTextLabel: UILabel = {
+        let label = UILabel()
         label.numberOfLines = 0
         label.text = club.clubContent
         label.font = .preferredFont(forTextStyle: .callout)
