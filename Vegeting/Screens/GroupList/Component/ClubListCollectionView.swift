b03767f53b9a6a891506aab3ab812ff730dc83ba
ccdkk <cdk243@gmail.com>
2022-11-24 17:25:14 +0900

    [del]: Task 삭제 (#153)

--- Vegeting/Screens/GroupList/Component/ClubListCollectionView.swift
+++ Vegeting/Screens/GroupList/Component/ClubListCollectionView.swift
@@ -59,13 +59,11 @@ class ClubListCollectionView: UICollectionView {
 
 extension ClubListCollectionView: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        Task {
             guard let currentUser = AuthManager.shared.getCurrentUser() else { return }
             let selectedClub = clubList[indexPath.item]
             let isMine = currentUser.userID == selectedClub.hostID
             let detailViewController = PostDetailViewController(club: clubList[indexPath.item], entryPoint: isMine ? .mine : .other)
             tapDelegate?.clubListCellTapped(viewController: detailViewController)
-        }
     }
 }
 
