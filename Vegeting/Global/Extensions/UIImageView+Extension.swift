ae9817f7266f0661549d46679e524f9429e8917e
kelly1422 <kelly1422@naver.com>
2022-11-28 13:44:39 +0900

    [chore]: 주석 삭제 (#142)

--- Vegeting/Global/Extensions/UIImageView+Extension.swift
+++ Vegeting/Global/Extensions/UIImageView+Extension.swift
@@ -12,14 +12,14 @@ import Kingfisher
 extension UIImageView {
     func setImage(with url: URL) {
         let cache = ImageCache.default
-        cache.retrieveImage(forKey: url.absoluteString, options: nil) { result in // 캐시에서 키를 통해 이미지를 가져온다.
+        cache.retrieveImage(forKey: url.absoluteString, options: nil) { result in
             switch result {
             case .success(let value):
-                if let image = value.image { // 만약 캐시에 이미지가 존재한다면
-                    self.image = image // 바로 이미지를 셋한다.
+                if let image = value.image {
+                    self.image = image
                 } else {
-                    let resource = ImageResource(downloadURL: url, cacheKey: url.absoluteString) // URL로부터 이미지를 다운받고 String 타입의 URL을 캐시키로 지정하고
-                    self.kf.setImage(with: resource) // 이미지를 셋한다.
+                    let resource = ImageResource(downloadURL: url, cacheKey: url.absoluteString)
+                    self.kf.setImage(with: resource)
                 }
             case .failure(let error):
                 print(error)
