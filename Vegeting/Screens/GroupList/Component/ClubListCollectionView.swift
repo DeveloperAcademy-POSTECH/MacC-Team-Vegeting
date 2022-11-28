2eba9bcda165a5cbabbf33660942248430042335
kelly1422 <kelly1422@naver.com>
2022-11-28 13:45:55 +0900

    [chore]: final 키워드 추가 (#142)

--- Vegeting/Screens/GroupList/Component/ClubListCollectionView.swift
+++ Vegeting/Screens/GroupList/Component/ClubListCollectionView.swift
@@ -11,7 +11,7 @@ protocol ClubListCollectionViewDelegate: AnyObject {
     func clubListCellTapped(viewController: PostDetailViewController)
 }
 
-class ClubListCollectionView: UICollectionView {
+final class ClubListCollectionView: UICollectionView {
     private var clubList: [Club] = [] {
         didSet {
             DispatchQueue.main.async { [weak self] in
