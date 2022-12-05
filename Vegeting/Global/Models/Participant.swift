33d7959c47186c1444ccf21a03dc585d97097ff8
ccdkk <cdk243@gmail.com>
2022-12-05 21:31:31 +0900

    [chore]: 모델 옵셔널로 변경 (#167)

--- Vegeting/Global/Models/Participant.swift
+++ Vegeting/Global/Models/Participant.swift
@@ -10,11 +10,11 @@ import Foundation
 struct Participant: Codable {
     let userID: String?
     let name: String
-    let birth: Int
-    let location: String
-    let gender: String
-    let vegetarianType: String
-    let introduction: String
-    let interests: [String]
+    let birth: Int?
+    let location: String?
+    let gender: String?
+    let vegetarianType: String?
+    let introduction: String?
+    let interests: [String]?
     var profileImageURL: String?
 }
