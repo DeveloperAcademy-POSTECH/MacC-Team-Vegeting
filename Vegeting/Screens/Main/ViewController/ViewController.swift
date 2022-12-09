cc995daba793679dba2e52cbff82870bcc609d08
ccdkk <cdk243@gmail.com>
2022-12-09 21:16:17 +0900

    [feat]: lottie spm 추가 & launch ViewController 연결 (#212)

--- Vegeting/Screens/Main/ViewController/ViewController.swift
+++ Vegeting/Screens/Main/ViewController/ViewController.swift
@@ -7,11 +7,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+    
+    let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "eventDetail")
+        return imageView
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        
+        tabBarController?.tabBar.isHidden = true
+        setupLayout()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1, execute: {
+            NotificationCenter.default.post(name: NSNotification.Name("didLaunchScreenEnded"), object: nil)
+        })
     }
+    
+    private func setupLayout() {
+        view.addSubview(imageView)
+        imageView.constraint(top: view.topAnchor,
+                             leading: view.leadingAnchor,
+                             bottom: view.safeAreaLayoutGuide.bottomAnchor,
+                             trailing: view.trailingAnchor)
+    }
+    
 }
 
